e4dc6a1cbf48870e0cf9e454c520e2e3954f8200
Duy Le <duy@Duys-MacBook-Air.local>
2022-10-10 12:01:04 -0400

    added homepage

---
+++ hooks/useInput.js
@@ -0,0 +1,86 @@
+import { useState, useEffect, useRef } from 'react'
+import { getPokemonGeneration, isNumber } from '../utils/helpers'
+import { getAllPokemonNames } from '../utils/axios'
+import { useRouter } from 'next/router'
+
+export const useInput = () => {
+	const [pokemon, setPokemon] = useState([])
+	const [filteredList, setFilteredList] = useState([])
+	const [activeIndex, setActiveIndex] = useState(0)
+	const log = useRef(true)
+
+	const router = useRouter()
+
+	useEffect(() => {
+		if (log.current) {
+			const fetchData = async () => {
+				const res = await getAllPokemonNames()
+				setPokemon(res)
+			}
+			fetchData()
+		}
+
+		return () => {
+			log.current = false
+		}
+	}, [])
+
+	const onChangeHandler = e => {
+		setActiveIndex(0)
+		const input = e.target.value
+
+		//when input field is empty
+		if (input.length === 0) {
+			setFilteredList([])
+			return
+		}
+
+		if (isNumber(input)) {
+			if (input <= 0) {
+				return
+			}
+			setFilteredList([pokemon[input - 1]])
+		} else {
+			const searchItem = input.toLowerCase() //auto capitalize on mobile
+			const copy = [...pokemon]
+			const results = copy.sort().filter(pkmn => pkmn.startsWith(searchItem))
+			results.splice(5, copy.length)
+			setFilteredList(results)
+		}
+	}
+
+	const onKeyDownHandler = e => {
+		if (e.key === 'Enter') {
+			const highlightedPokemon = filteredList[activeIndex]
+			const pokemonId = pokemon.indexOf(highlightedPokemon) + 1
+			const gen = getPokemonGeneration(pokemonId)
+			router.push(`/gen/${gen}/${highlightedPokemon}`)
+			onCloseHandler()
+		} else if (e.key === 'ArrowUp') {
+			if (activeIndex === 0) {
+				return
+			}
+			setActiveIndex(activeIndex - 1)
+		} else if (e.key === 'ArrowDown') {
+			if (activeIndex === filteredList.length - 1) {
+				return
+			}
+			setActiveIndex(activeIndex + 1)
+		}
+	}
+
+	const onCloseHandler = () => {
+		setActiveIndex(0)
+		setFilteredList([])
+	}
+
+	return {
+		pokemon,
+		filteredList,
+		activeIndex,
+		setActiveIndex,
+		onChangeHandler,
+		onKeyDownHandler,
+		onCloseHandler
+	}
+}
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,122 @@
-import { Heading } from '@chakra-ui/react'
+import {
+	chakra,
+	Box,
+	Flex,
+	Input,
+	InputGroup,
+	InputLeftElement,
+	Text,
+	Button,
+	SimpleGrid,
+	GridItem,
+	Divider,
+	HStack,
+	Container
+} from '@chakra-ui/react'
+import { useInput } from '../hooks/useInput'
+import { SearchIcon } from '@chakra-ui/icons'
+import { getPokemonGeneration } from '../utils/helpers'
+import Layout from '../components/layout'
+import SearchResultList from '../components/navbar/search-result-list'
 
+const regions = [
+	'Gen 1',
+	'Gen 2',
+	'Gen 3',
+	'Gen 4',
+	'Gen 5',
+	'Gen 6',
+	'Gen 7',
+	'Gen 8'
+]
 const Page = () => {
+	const {
+		pokemon,
+		filteredList,
+		activeIndex,
+		setActiveIndex,
+		onChangeHandler,
+		onKeyDownHandler,
+		onCloseHandler
+	} = useInput()
+
 	return (
-		<>
-			<Heading as='h1' size='xl' align='center' py={4}>
-				Home Page
-			</Heading>
-			<Heading as='h1' size='md' align='center' py={4}>
-				Nintendo has been creating a lot of questionable Pokemon. Luckily they
-				are looking for your feedback.
-			</Heading>
-		</>
+		<Layout>
+			<Container maxW='5xl' pt={40} h='98vh'>
+				<Box pt={20} textAlign={{ base: 'left', md: 'center' }}>
+					<Container maxW='xl'>
+						<chakra.span
+							fontSize={{ base: '2xl', sm: '4xl' }}
+							fontWeight='extrabold'
+							letterSpacing='tight'
+							lineHeight='shorter'
+							color='gray.100'
+							mb={6}
+						>
+							<chakra.span display='block' color='gray.500'>
+								Ready to leave a review?
+							</chakra.span>
+							<chakra.span display='inline'>
+								Find your{' '}
+								<Text
+									display={{
+										base: 'inline',
+										lg: 'inline'
+									}}
+									w='full'
+									bgClip='text'
+									bgGradient='linear(to-r, green.400,purple.500)'
+									fontWeight='extrabold'
+								>
+									favorite Pokemon
+								</Text>{' '}
+							</chakra.span>
+						</chakra.span>
+						<HStack mt={8} justify='center' w='full'>
+							<InputGroup bg='gray.700' rounded='md' size='lg'>
+								<InputLeftElement pointerEvents='none' position='relative'>
+									<SearchIcon color='brand.400' />
+								</InputLeftElement>
+								<Input
+									p={0}
+									variant='unstyled'
+									placeholder='Search by name or number'
+									onChange={e => onChangeHandler(e)}
+									onKeyDown={e => onKeyDownHandler(e)}
+								/>
+							</InputGroup>
+
+							<Button size='lg'>Random</Button>
+						</HStack>
+						{filteredList.map((pkmn, index) => {
+							const pokemonId = pokemon.indexOf(pkmn) + 1
+							const gen = getPokemonGeneration(pokemonId)
+							return (
+								<SearchResultList
+									gen={gen}
+									activeIndex={activeIndex}
+									setActiveIndex={setActiveIndex}
+									key={pkmn}
+									pokemon={pkmn}
+									onClose={() => onCloseHandler()}
+									index={index}
+								/>
+							)
+						})}
+						<Divider my={20} />
+						<Flex as='section' justify='center' w='full' h='full'>
+							<SimpleGrid columns={[2, 4, 4]} spacing={10}>
+								{regions.map(region => (
+									<GridItem key={region}>
+										<Button>{region}</Button>
+									</GridItem>
+								))}
+							</SimpleGrid>
+						</Flex>
+					</Container>
+				</Box>
+			</Container>
+		</Layout>
 	)
 }
 export default Page
